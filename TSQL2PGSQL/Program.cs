6c7b52782dc82aac0ccbb2ddfe3160cb3197554a
Dmitry Kolchev <dmitrykolchev@msn.com>
2017-09-25 20:04:37 +0300

    CLI

--- TSQL2PGSQL/Program.cs
+++ TSQL2PGSQL/Program.cs
@@ -13,15 +13,44 @@ namespace DykBits
     {
         static void Main(string[] args)
         {
-            Console.OutputEncoding = Encoding.UTF8;
-            TSql130Parser parser = new TSql130Parser(false);
-            using (FileStream stream = File.Open("script.sql", FileMode.Open, FileAccess.Read, FileShare.Read))
+            try
             {
-                StreamReader reader = new StreamReader(stream);
-                TSqlFragment fragment = parser.Parse(reader, out IList<ParseError> errors);
-                PgTranslatorVisitor translator = new PgTranslatorVisitor();
-                fragment.Accept(translator);
-                Console.WriteLine(translator.Buffer.ToString());
+                if (args.Length != 2 && args.Length != 1)
+                {
+                    throw new ArgumentException("", nameof(args));
+                }
+                Console.OutputEncoding = Encoding.UTF8;
+                TSql130Parser parser = new TSql130Parser(false);
+                using (FileStream stream = File.Open(args[0], FileMode.Open, FileAccess.Read, FileShare.Read))
+                {
+                    StreamReader reader = new StreamReader(stream);
+                    TSqlFragment fragment = parser.Parse(reader, out IList<ParseError> errors);
+                    PgTranslatorVisitor translator = new PgTranslatorVisitor();
+                    fragment.Accept(translator);
+                    if (args.Length == 2)
+                    {
+                        using (StreamWriter output = File.CreateText(args[1]))
+                        {
+                            output.Write(translator.Buffer.ToString());
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine(translator.Buffer.ToString());
+                    }
+                }
+                Environment.ExitCode = 0;
+            }
+            catch (ArgumentException ex) when (ex.ParamName == nameof(args))
+            {
+                Console.WriteLine("Usage:");
+                Console.WriteLine();
+                Console.WriteLine("TSQL2PGSQL inputfile [outputfile]");
+                Console.WriteLine();
+                Console.WriteLine("Parameters:");
+                Console.WriteLine("\tinputfile  - T-SQL script input file name");
+                Console.WriteLine("\toutputfile - PostgreSQL script output file name, if omitted output to console");
+                Environment.ExitCode = ex.HResult;
             }
         }
     }
